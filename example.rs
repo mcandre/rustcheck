6f2406a40f9c0734efc096ff6590244805ac4785
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-09-15 16:50:50 -0400

    tabs

--- example.rs
+++ example.rs
@@ -4,21 +4,21 @@ import rustcheck::*;
 use std;
 
 fn prop_even(x : int) -> bool {
-	ret x % 2 == 0;
+  ret x % 2 == 0;
 }
 
 fn gen_even() -> int {
-	let i : int = rustcheck::gen_int();
+  let i : int = rustcheck::gen_int();
 
-	if i % 2 == 0 {
-		ret i;
-	}
-	else {
-		ret i + 1;
-	}
+  if i % 2 == 0 {
+    ret i;
+  }
+  else {
+    ret i + 1;
+  }
 }
 
 fn main() {
-	for_all(prop_even, [gen_int]);
-	for_all(prop_even, [gen_even]);
+  for_all(prop_even, [gen_int]);
+  for_all(prop_even, [gen_even]);
 }
